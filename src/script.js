2675a93ff10087ad4424d16a0296145d4395d4b2
Sohel <152502004+5OH3L@users.noreply.github.com>
2025-07-24 17:50:45 +0530

    Code functions for fetching weather data & processing

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
-import './styles.css'
+import "./styles.css";
+
+function formatData(json) {
+  const keys = ["resolvedAddress", "description", "days", "currentConditions"];
+  let formattedData = {};
+  keys.forEach((key) => {
+    if (json.hasOwnProperty(key)) {
+      formattedData[key] = json[key];
+    }
+  });
+  return formattedData;
+}
+async function getWeather(location) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=KEY&contentType=json`
+  );
+  const json = await response.json();
+  return json;
+}
