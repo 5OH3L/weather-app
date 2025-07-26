dca5eee1ca4ee65546d6dde464d178c791dde81c
Sohel <152502004+5OH3L@users.noreply.github.com>
2025-07-26 17:28:10 +0530

    Desing days/hours UI

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+import { get } from "jquery";
 import "./styles.css";
 
 function formatData(json) {
@@ -12,7 +13,7 @@ function formatData(json) {
 }
 async function getWeather(location) {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=KEY&contentType=json`
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=68VQNA672BC6B3AQ56V4JGL7J&contentType=json&iconSet=icons2`
   );
   const json = await response.json();
   return json;
